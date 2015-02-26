f16e86392456f164d558e412661501602ff706fc
Tom Rosier <thr2@aber.ac.uk>
2015-02-26 19:41:54 +0000

    first commit

---
+++ app/src/main/java/uk/co/tomrosier/xetk/losesono/prototype/prototype/GPSObj.java
@@ -0,0 +1,19 @@
+package uk.co.tomrosier.xetk.losesono.prototype.prototype;
+
+import java.util.Date;
+
+/**
+ * Created by xetk on 26/02/15.
+ */
+public class GPSObj {
+
+    int userID;
+    int[] friendID;
+
+    int longitude;
+    int latitude;
+    int range;
+
+    String message;
+    Date date;
+}
---
+++ app/src/main/java/uk/co/tomrosier/xetk/losesono/prototype/prototype/MainActivity.java
@@ -0,0 +1,105 @@
+package uk.co.tomrosier.xetk.losesono.prototype.prototype;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    Button btnGetGPS;
+    Button btnSend;
+
+    TextView lblGPSLoc;
+
+    EditText txtUserName;
+
+    GPSTracker gps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGetGPS   = (Button)   findViewById(R.id.btnGetGPS);
+        btnSend     = (Button)   findViewById(R.id.btnSend);
+        lblGPSLoc   = (TextView) findViewById(R.id.lblGPSLoc);
+        txtUserName = (EditText) findViewById(R.id.txtUserName);
+
+        // Show location button click event
+        btnGetGPS.setOnClickListener(
+            new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+                    // Create class object
+                    gps = new GPSTracker(MainActivity.this);
+
+                    // Check if GPS enabled
+                    if(gps.canGetLocation()) {
+
+                        double latitude = gps.getLatitude();
+                        double longitude = gps.getLongitude();
+
+                        // \n is for new line
+                        if (latitude != 0 && longitude != 0) {
+                            lblGPSLoc.setText("Your Location is - \nLat: " + latitude + "\nLong: " + longitude);
+                        } else {
+                            lblGPSLoc.setText("No fix yet");
+                        }
+                    } else {
+                        // Can't get location.
+                        // GPS or network is not enabled.
+                        // Ask user to enable GPS/network in settings.
+                        gps.showSettingsAlert();
+                    }
+                }
+            }
+        );
+
+
+        btnSend.setOnClickListener(
+            new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+
+                    String username = txtUserName.getText().toString();
+
+                    Toast.makeText(getApplicationContext(), "Submitting Location To Database. For user: " + username, Toast.LENGTH_LONG).show();
+
+                }
+            }
+        );
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
