e943b077b143feb77858c042d5814ebd128c750d
Tom Rosier <thr2@aber.ac.uk>
2015-04-16 22:53:34 +0100

    Commented alot of the restclients

--- app/src/main/java/uk/co/tomrosier/xetk/losesono/prototype/prototype/RestClient/RestClient.java
+++ app/src/main/java/uk/co/tomrosier/xetk/losesono/prototype/prototype/RestClient/RestClient.java
@@ -13,61 +13,77 @@ import java.security.KeyStore;
 import uk.co.tomrosier.xetk.losesono.prototype.prototype.Constants;
 
 /**
- * Created by xetk on 05/03/15.
+ * This is the core of dealing with HTTP REST requests, it enables different types of requests to the backend services.
  */
 public class RestClient {
 
+    // Get the base URL that we work with, then add the endpoints that we want to work with.
     private static final String BASE_URL = Constants.BASE_URL;
 
+    // Keep the HTTP client alive while we are doing RESTful requests.
     private AsyncHttpClient client = new AsyncHttpClient();
 
+    // Default constructor where we can pass in the context which will stick our cookies to to keep the application authenticated.
     public RestClient(Context context) {
 
+        // Setup a SSL connection to ensure our data is secure.
         setupSSL();
 
+        // This is global store for all the cookies within the application, we pin it to the context so we can use it in different locations within the application.
         PersistentCookieStore cookieStore = new PersistentCookieStore(context);
         client.setCookieStore(cookieStore);
-
     }
 
+    // Setup a SSL connection for none cookie dependant connections, so we still have SSL\TLS security on the connection.
     public RestClient() {
         setupSSL();
     }
 
-
+    // Configure a SSL\TLS protected connection ready for our data to be exchanged.
     private void setupSSL() {
+
         try {
             KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
 
             trustStore.load(null, null);
 
+            // This is the key factory it stores all the encryption keys needed for the SSL to work correctly.
             MySSLSocketFactory sf = new MySSLSocketFactory(trustStore);
 
             sf.setHostnameVerifier(MySSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
 
+            // Attach the certificates to the http client.
             client.setSSLSocketFactory(sf);
         } catch (Exception e) {
 
         }
     }
 
+    // Do a get request to the server, with all the parameters setup.
     public void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
 
+        // Join the URL together to get the correct endpoint.
         String uri = getCompleteURL(url);
 
         System.out.println("Rest Get URL: " + uri);
 
+        // Do a get request to the server.
         client.get(uri, params, responseHandler);
     }
 
+    // Same again for POST requests.
     public void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+
+        // Join the URL's together so we have the correct URL for the endpoint.
         String uri = getCompleteURL(url);
 
         System.out.println("Rest Post URL: " + uri);
 
+        // Post the data to the server.
         client.post(uri, params, responseHandler);
     }
 
+    // Join the BASE_URL so the address of the server to the end point link.
     public static String getCompleteURL(String relURL) {
         return BASE_URL + '/' + relURL;
     }
--- app/src/main/java/uk/co/tomrosier/xetk/losesono/prototype/prototype/RestClient/UserRestClient.java
+++ app/src/main/java/uk/co/tomrosier/xetk/losesono/prototype/prototype/RestClient/UserRestClient.java
@@ -13,43 +13,55 @@ import uk.co.tomrosier.xetk.losesono.prototype.prototype.entities.User;
 import uk.co.tomrosier.xetk.losesono.prototype.prototype.utils.AjaxCompleteHandler;
 
 /**
- * Created by xetk on 05/03/15.
+ * This is the Rest Client for dealing with interaction with user objects on the server.
  */
 public class UserRestClient  {
 
-    RestClient restClient;
+    // Declare the global Rest Client we interact with.
+    private RestClient restClient;
 
+    // Declare the Rest client and pass the context so we have cookies to work with.
     public UserRestClient(Context context) {
         restClient = new RestClient(context);
     }
 
-
+    // Build the URL needed to get the user by id, then call the mater method to process the information.
     public void getUserByID(Integer id, final AjaxCompleteHandler handler) {
         userRequest("user/id/" + id, handler);
     }
 
+    // Build the URL needed to get the user by username, then call the mater method to process the information.
     public void getUserByName(String userID, final AjaxCompleteHandler handler) {
         userRequest("user/username/" + userID, handler);
     }
 
+    // This is getting the user object for the current user that is logged in.
     public void getUser(final AjaxCompleteHandler handler) {
         userRequest("user", handler);
     }
 
+    // This handles the requests with the user rest clients, dealing with the AJAX handling and objects.
     private void userRequest(String url, final AjaxCompleteHandler handler) {
+
+        // Get the information from the server.
         restClient.get(
                 url,
                 null,
                 new JsonHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                        // If we got the data successfully.
                         if (statusCode == 200) {
                             try {
+                                // For each of the users we get back, we call the ajax handler, to process the responce.
                                 for (int i = 0; i < response.length(); i++) {
+                                    // Get the JSON object from the JSON array to manipulate
                                     JSONObject obj = response.getJSONObject(i);
 
+                                    // Convert the JSONObject into a actual user object that then can be used.
                                     User userObj = new User(obj);
 
+                                    // Pass that user object back to the AJAX handler.
                                     handler.handleAction(userObj);
                                 }
                             } catch (JSONException e) {
@@ -57,6 +69,7 @@ public class UserRestClient  {
                             }
 
                         } else {
+                            // Sometimes things break.
                             System.err.println("Getting Messages failed with status code of " + statusCode);
                         }
                     }
