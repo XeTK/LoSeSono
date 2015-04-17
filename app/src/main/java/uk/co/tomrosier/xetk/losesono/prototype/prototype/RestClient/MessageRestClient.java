7840ae81abb7d014a6b6b850e7a8686e5a995e22
Tom Rosier <thr2@aber.ac.uk>
2015-04-17 11:02:51 +0100

    Commented models.

--- app/src/main/java/uk/co/tomrosier/xetk/losesono/prototype/prototype/RestClient/MessageRestClient.java
+++ app/src/main/java/uk/co/tomrosier/xetk/losesono/prototype/prototype/RestClient/MessageRestClient.java
@@ -16,16 +16,19 @@ import uk.co.tomrosier.xetk.losesono.prototype.prototype.entities.Message;
 import uk.co.tomrosier.xetk.losesono.prototype.prototype.utils.AjaxCompleteHandler;
 
 /**
- * Created by xetk on 05/03/15.
+ * This is the HTTP REST Client it deals with receiving and processing messages within the applications.
  */
 public class MessageRestClient {
 
-    RestClient restClient;
+    // Keep the higher REST Client object ready for us to access it.
+    private RestClient restClient;
 
+    // Create a instance of the REST Client when this class is instantiated.
     public MessageRestClient(Context context) {
         restClient = new RestClient(context);
     }
 
+    // This gets a message by the message_id, and converting it to Java message objects.
     public void getMessageByID(Integer id, final AjaxCompleteHandler handler) {
 
         restClient.get(
@@ -34,10 +37,13 @@ public class MessageRestClient {
                 new JsonHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        // If the request returns succesfully then we continue to process the messages.
                         if (statusCode == 200) {
                             try {
+                                // Convert the responce into a Java Message object.
                                 Message msg = new Message(response);
 
+                                // Pass the message back to the method that called to get messages.
                                 handler.handleAction(msg);
                             } catch (JSONException e) {
                                 e.printStackTrace();
@@ -56,6 +62,7 @@ public class MessageRestClient {
         );
     }
 
+    // Get all the messages for the current user that is signed in.
     public void getMessagesForUser(final AjaxCompleteHandler handler) {
 
         restClient.get(
@@ -64,7 +71,9 @@ public class MessageRestClient {
             new JsonHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                    // If the status is successful, then we continue.
                     if (statusCode == 200) {
+                        // Pass the response to the Message Model to process and handle within the UI.
                         MessageModel.processMessages(response, handler);
                     } else {
                         System.err.println("Getting Messages failed with status code of " + statusCode);
@@ -74,6 +83,7 @@ public class MessageRestClient {
         );
     }
 
+    // Get all the messages that are related to the user that is currently logged in.
     public void getMessagesForFriends(final AjaxCompleteHandler handler) {
 
         restClient.get(
@@ -83,6 +93,7 @@ public class MessageRestClient {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                         if (statusCode == 200) {
+                            // Pass the response to the Message Model to process and handle within the UI.
                             MessageModel.processMessages(response, handler);
                         } else {
                             System.err.println("Getting Messages failed with status code of " + statusCode);
@@ -92,6 +103,7 @@ public class MessageRestClient {
         );
     }
 
+    // This gets the message that are needed for the notificiations within the application.
     public void getMessagesForNotifications(final AjaxCompleteHandler handler) {
 
         restClient.get(
@@ -101,6 +113,7 @@ public class MessageRestClient {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                         if (statusCode == 200) {
+                            // This processes the responce within the Message Model,
                             MessageModel.processMessages(response, handler);
                         } else {
                             System.err.println("Getting Messages failed with status code of " + statusCode);
@@ -110,16 +123,20 @@ public class MessageRestClient {
         );
     }
 
+    // This calls adding a message within the back end of the application.
     public void addMessage(final Activity activity, Message message) {
 
+        // Define a list of parameters to pass over the POST request.
         RequestParams params = new RequestParams();
 
+        // Adding the parameters for building a message.
         params.put("private", message.isPrivate());
         params.put("content", message.getContent());
         params.put("longitude", message.getLongitude());
         params.put("latitude", message.getLatitude());
         params.put("range", message.getRange());
 
+        // Send the request off to the backend of the application.
         restClient.post(
                 "message/add/message",
                 params,
@@ -127,6 +144,7 @@ public class MessageRestClient {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                         if (statusCode == 200) {
+                            // Process the message, and handle the other activities.
                             MessageModel.addMessage(response, activity);
                         } else {
                             System.err.println("Getting Messages failed with status code of " + statusCode);
@@ -136,22 +154,26 @@ public class MessageRestClient {
         );
     }
 
+    // This adds a user to message and makes it viewable to other people.
     public void addUser(final AjaxCompleteHandler handler, int messageID, int friendLinkID) {
 
+        // This is the parameters that need to be passed to the backend application.
         RequestParams params = new RequestParams();
 
         params.put("friend_id", friendLinkID);
         params.put("message_id", messageID);
 
-
+        // Send this info to the server.
         restClient.post(
                 "message/add/user",
                 params,
                 new JsonHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        // If the response returns successfully.
                         if (statusCode == 200) {
                             try {
+                                // Pass the group_id back to the method that called adding a user to a message.
                                 handler.handleAction(response.getInt("group_id"));
                             } catch (JSONException e) {
                                 e.printStackTrace();
@@ -164,21 +186,25 @@ public class MessageRestClient {
         );
     }
 
+    // This handles when a message is read, so we dont get notified multiple times for a message that has been read.
     public void addMessageRead(int messageID, final AjaxCompleteHandler handler) {
 
+        // Pass the message_id to the server so that we can log it has been read.
         RequestParams params = new RequestParams();
 
         params.put("message_id", messageID);
 
-
+        // Send the details of the REST request over to the server to log it as read.
         restClient.post(
                 "message/read",
                 params,
                 new JsonHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        // If the status is returned successfully.
                         if (statusCode == 200) {
                             try {
+                                // Pass the group_id even if we dont use it back to the method that called it.
                                 handler.handleAction(response.getInt("group_id"));
                             } catch (JSONException e) {
                                 e.printStackTrace();
--- app/src/main/java/uk/co/tomrosier/xetk/losesono/prototype/prototype/RestModel/MessageModel.java
+++ app/src/main/java/uk/co/tomrosier/xetk/losesono/prototype/prototype/RestModel/MessageModel.java
@@ -12,21 +12,25 @@ import uk.co.tomrosier.xetk.losesono.prototype.prototype.entities.Message;
 import uk.co.tomrosier.xetk.losesono.prototype.prototype.utils.AjaxCompleteHandler;
 
 /**
- * Created by xetk on 06/03/15.
+ * This handles the processing of messages and converting them into a usable format.
  */
 public class MessageModel {
 
-
+    // This processes the messages and converting them into something useable in the ui.
     public static void processMessages(JSONArray response, final AjaxCompleteHandler handler) {
 
         try {
 
+            // Loop through all of the message objects that have been returned.
             for (int i = 0; i < response.length(); i++) {
 
+                // Get the JSONObject from the array.
                 JSONObject obj = response.getJSONObject(i);
 
+                // Convert a JSON object into a Java Message object that we can then use.
                 Message msg = new Message(obj);
 
+                // Pass the object back so we can use it.
                 handler.handleAction(msg);
             }
         } catch (JSONException e) {
@@ -34,10 +38,14 @@ public class MessageModel {
         }
     }
 
+    // This handles adding a message for a given user.
     public static void addMessage(JSONObject response, Activity activity) {
 
         try {
+            // Close the activity once we are done processing the response.
             activity.finish();
+
+            // Open the activity to enable adding people that can view the message.
             Intent myIntent = new Intent(activity, MessageFriendsActivity.class);
             myIntent.putExtra("Message_ID", response.getInt("message_id"));
             activity.startActivity(myIntent);
@@ -45,9 +53,6 @@ public class MessageModel {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-
-        //Toast.makeText(activity.getApplicationContext(), response.toString(),Toast.LENGTH_LONG).show();
     }
 
 }
